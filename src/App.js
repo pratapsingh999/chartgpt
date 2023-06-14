7c2b52a3ff26660daaa6ee49dc77249c939cd1c9
pratapsingh999 <108220183+pratapsingh999@users.noreply.github.com>
2023-06-14 11:39:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm ChatGPT! Ask me anything!",
+      // sentTime: "just now",
+      sender: "ChatGPT"
+    }
+  ]);
+
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message : message, 
+      direction: 'outgoing',
+      sender: "user"
+    };
+
+    const newMessages = [...messages , newMessage];
+    setMessages(newMessages);
+    
+// Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
+    setIsTyping(true);
+    // await processMessageToChatGPT(newMessages);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+      <MainContainer>
+        <ChatContainer>       
+          <MessageList 
+            scrollBehavior="smooth" 
+            typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+          >
+            {messages.map((message, i) => {
+              console.log(message)
+              return <Message key={i} model={message} />
+            })}
+          </MessageList>
+          <MessageInput placeholder="Type message here" onSend={handleSend} />        
+        </ChatContainer>
+      </MainContainer>
     </div>
+  </div>
   );
 }
 
